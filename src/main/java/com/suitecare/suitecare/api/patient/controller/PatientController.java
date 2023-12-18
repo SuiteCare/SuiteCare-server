0607b6e017c50f1e67d7299c8a1b47791090e58f
DojinP <coah2013@naver.com>
2023-12-18 15:41:29 +0900

    [feat] Patient 도메인 추가
    - Patient 도메인 추가

---
+++ src/main/java/com/suitecare/suitecare/api/patient/controller/PatientController.java
@@ -0,0 +1,13 @@
+package com.suitecare.suitecare.api.patient.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/v1")
+@CrossOrigin
+public class PatientController {
+
+
+}
