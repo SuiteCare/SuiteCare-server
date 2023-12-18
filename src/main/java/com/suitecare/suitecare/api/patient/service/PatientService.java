916db78a8eb6895aca5d7e8beae148213dbabb3c
DojinP <coah2013@naver.com>
2023-12-18 21:22:18 +0900

    [feat] Patient, Reservation 도메인 추가
    - Patient, Reservation 도메인 추가

---
+++ src/main/java/com/suitecare/suitecare/api/patient/service/PatientService.java
@@ -0,0 +1,7 @@
+package com.suitecare.suitecare.api.patient.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PatientService {
+}
---
+++ src/main/java/com/suitecare/suitecare/api/reservation/controller/ReservationController.java
@@ -0,0 +1,17 @@
+package com.suitecare.suitecare.api.reservation.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/v1")
+@CrossOrigin
+public class ReservationController {
+/*
+
+    @GetMapping("reservation")
+    public List<>
+*/
+}
