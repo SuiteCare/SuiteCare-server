a15fa04aed94eecfca2a0cf917d9970df218d1a7
DojinP <coah2013@naver.com>
2023-12-18 15:40:51 +0900

    [refactor] 파일구조 변경
    - family 도메인 추가
    - 구분자 파일 제거

---
+++ src/main/java/com/suitecare/suitecare/api/family/controller/FamilyController.java
@@ -0,0 +1,12 @@
+package com.suitecare.suitecare.api.family.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/v1")
+@CrossOrigin
+public class FamilyController {
+
+}
