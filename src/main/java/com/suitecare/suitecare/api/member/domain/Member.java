887ff4c0874c207abdc0f0a0e882cb023e5c57c2
DojinP <coah2013@naver.com>
2023-12-21 18:19:59 +0900

    [feat] JWT
    - JJWT 의존성 추가
    - JwtUtils 클래스 추가

---
+++ src/main/java/com/suitecare/suitecare/api/member/domain/Member.java
@@ -0,0 +1,29 @@
+package com.suitecare.suitecare.api.member.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+import lombok.NonNull;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+
+@Data
+public class Member {
+    @NonNull
+    private Long id;
+    private String login_id;
+    private String password;
+    private String name;
+    private String gender;
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
+    private Date birthday;
+    private String email;
+    private String tel;
+    private String address;
+    private String address_detail;
+    private String role;
+    @JsonFormat(pattern = "yyyy.MM.dd'T'hh:mm", timezone = "Asia/Seoul")
+    private Timestamp create_at;
+    @JsonFormat(pattern = "yyyy.MM.dd'T'hh:mm", timezone = "Asia/Seoul")
+    private Timestamp update_at;
+}
