bf3f37408a9b596c58534497f22543a25f2de233
Kim Hyun Hee <hhkim0123@naver.com>
2023-12-28 17:37:06 +0900

    [fix] Mypage 비밀 번호 변경 DB 연동 수정

--- src/main/java/com/suitecare/suitecare/api/member/dto/ChangePwRequestDTO.java
+++ src/main/java/com/suitecare/suitecare/api/member/dto/ChangePwRequestDTO.java
@@ -3,6 +3,7 @@ package com.suitecare.suitecare.api.member.dto;
 import lombok.Data;
 
 @Data
-public class ChangePwResponseDTO {
+public class ChangePwRequestDTO {
+    private int login_id;
     private String password;
 }
