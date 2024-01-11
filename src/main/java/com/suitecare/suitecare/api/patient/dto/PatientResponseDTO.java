8598b018ead3de9c9f40b8f35450901798bd0c37
Lee Jae Sun <ze_s96@naver.com>
2024-01-11 17:31:09 +0900

    [refactor] 환자 정보 DTO 수정

--- src/main/java/com/suitecare/suitecare/api/patient/dto/PatientResponseDTO.java
+++ src/main/java/com/suitecare/suitecare/api/patient/dto/PatientResponseDTO.java
@@ -6,11 +6,10 @@ import java.sql.Date;
 
 @Data
 public class PatientResponseDTO {
+    private String name;
     private String diagnosis_name;
     private String gender;
     private Date birthday;
-    private Date start_date;
-    private Date end_date;
-    private String[] day_of_week;
-    private int wage;
+    private String height;
+    private String weight;
 }
