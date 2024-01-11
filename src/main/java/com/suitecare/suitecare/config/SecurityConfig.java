6e2919cff7f9c4434b2d5b760f347cf4fa8cb47b
DojinP <coah2013@naver.com>
2024-01-11 19:34:37 +0900

    [feat] jwt 인증 구현 및 SecurityConfig 구조 변경
    - JwtAuthenticationFilter 구현
    - JwtAuthenticationFilter 적용
    - SecurityConfig 코드 최적화 및 로직 구조 변경

--- src/main/java/com/suitecare/suitecare/config/SecurityConfig.java
+++ src/main/java/com/suitecare/suitecare/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.suitecare.suitecare.config;
 
+import com.suitecare.suitecare.api.custom.filter.JwtAuthenticationFilter;
+import com.suitecare.suitecare.api.jwt.JwtUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,38 +10,34 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final String[] allowedUrls = {"/", "/swagger-ui/**", "/v3/**", "/api/v1/login", "/api/v1/signup"};
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
+    private final JwtUtils jwtUtils;
+
+    public SecurityConfig(JwtUtils jwtUtils) {
+        this.jwtUtils = jwtUtils;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable);
-        http
-                .authorizeHttpRequests(
-                        auth -> auth
-                                .requestMatchers("/api/v1/login").permitAll()
-                                .requestMatchers("/api/v1/member").permitAll()
-                                .requestMatchers("/api/v1/mypage").permitAll()
-                                .requestMatchers("/api/v1/changepw").permitAll()
-                                .requestMatchers("/api/v1/modify").permitAll()
-                                .requestMatchers("/api/v1/patient").permitAll()
-                                .requestMatchers("/api/v1/mate/profile").permitAll()
-                                .requestMatchers("/api/v1/patient/**").permitAll()
-                                .requestMatchers("/api/v1/patientDetail/**").permitAll()
-                                .requestMatchers("/api/v1/search-reservation").permitAll()
-                                .requestMatchers("/swagger-ui/**").permitAll()
-                                .requestMatchers("/v3/api-docs/**").permitAll()
-                                .requestMatchers("/api/v1/reservation/**").permitAll()
-                                .anyRequest().authenticated()
-                );
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(allowedUrls).permitAll()
+                        .anyRequest().authenticated())
+                .addFilterBefore(new JwtAuthenticationFilter(jwtUtils), BasicAuthenticationFilter.class);
+
         return http.build();
     }
 }
