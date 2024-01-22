3018e3ce1034828b7c98fd5e8bbf2ea0e6423021
Lee Jae Sun <ze_s96@naver.com>
2024-01-22 12:12:07 +0900

    [feat] 간병 지원자 생일 정보 추가

--- src/main/java/com/suitecare/suitecare/api/reservation/dto/ApplicantInfoResponseDTO.java
+++ src/main/java/com/suitecare/suitecare/api/reservation/dto/ApplicantInfoResponseDTO.java
@@ -2,9 +2,12 @@ package com.suitecare.suitecare.api.reservation.dto;
 
 import lombok.Data;
 
+import java.time.LocalDate;
+
 @Data
 public class ApplicantInfoResponseDTO {
     private String name;
+    private LocalDate birthday;
     private String profile_picture_filename;
     private String introduction;
     private String contact_time_start;
