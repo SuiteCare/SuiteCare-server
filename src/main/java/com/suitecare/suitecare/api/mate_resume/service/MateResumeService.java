254e49fdf0d9e8a3e9a052acfb50037afabfb7f6
DojinP <coah2013@naver.com>
2024-02-28 15:25:01 +0900

    [feat] 이력서 등록 로직 수정
    - 경력, 이력이 없는 경우의 이력서 등록 가능

--- src/main/java/com/suitecare/suitecare/api/mate_resume/service/MateResumeService.java
+++ src/main/java/com/suitecare/suitecare/api/mate_resume/service/MateResumeService.java
@@ -28,10 +28,17 @@ public class MateResumeService {
         // 이력서 Insert
         mateResumeMapper.createMateResume(id, resume_dto.getMateResume());
 
-        // 경력, 자격증, 지역, 대표서비스 Insert
-        careerMapper.createCareer(id, resume_dto.getCareerList());
-        certificateMapper.createCertificate(id, resume_dto.getCertificateList());
+        // 경력 Insert
+        if(resume_dto.getCareerList() != null) {
+            careerMapper.createCareer(id, resume_dto.getCareerList());
+        }
+        // 자격증 Insert
+        if(resume_dto.getCertificateList() != null) {
+            certificateMapper.createCertificate(id, resume_dto.getCertificateList());
+        }
+        // 지역 Insert
         locationMapper.createLocation(id, resume_dto.getLocationList());
+        // 대표서비스 Insert
         mainSeviceMapper.createMainService(id, resume_dto.getMainServiceList());
 
         return 1;
