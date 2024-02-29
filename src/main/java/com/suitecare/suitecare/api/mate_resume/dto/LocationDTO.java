cb852c41078e9da108c49ad2c59580fa5298a003
DojinP <coah2013@naver.com>
2024-02-29 16:07:10 +0900

    [refactor] 이력서 업데이트 로직 리팩토링
    - 반복 코드 제거

--- src/main/java/com/suitecare/suitecare/api/mate_resume/dto/LocationDTO.java
+++ src/main/java/com/suitecare/suitecare/api/mate_resume/dto/LocationDTO.java
@@ -1,12 +1,13 @@
 package com.suitecare.suitecare.api.mate_resume.dto;
 
+import com.suitecare.suitecare.api.custom.ifc.DTO;
 import lombok.*;
 
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class LocationDTO {
+public class LocationDTO implements DTO {
     private Long id;
     private String name;
 }
