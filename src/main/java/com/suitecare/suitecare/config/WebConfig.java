165b6aeac7ccd3ae5d79b92f3bb2a8f144bfeed0
DojinP <coah2013@naver.com>
2024-03-28 18:00:02 +0900

    [feat] ResourceHandler 추가
    - 서버에 저장된 경로에 접근할 수 있도록 리소스 핸들러 추가

--- src/main/java/com/suitecare/suitecare/config/WebConfig.java
+++ src/main/java/com/suitecare/suitecare/config/WebConfig.java
@@ -10,7 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/images/**")
-                .addResourceLocations("classpath:/static/images/");
+                .addResourceLocations("file:///C:/resources/");
+//                .addResourceLocations("classpath:/static/images/");
     }
 
 }
